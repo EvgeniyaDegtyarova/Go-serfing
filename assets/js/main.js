116df9c50ea4e544a33abf73b6ac7b203a79a286
Evgeniya <jeni4ka2509@gmail.com>
2020-08-31 19:31:31 +0300

    finish website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// const { active } = require("browser-sync");
+
 $(function() {
     $('.header__slider').slick({
         infinite: true,
@@ -11,6 +13,10 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
+        responsive: [{
+            breakpoint: 961,
+            settings: "unslick"
+        }, ]
     });
     $('.surf-slider').slick({
         slidesToShow: 4,
@@ -18,55 +24,110 @@ $(function() {
         prevArrow: '<img class="slider-arrows slider-arrows__left"  src="assets/img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="assets/img/arrows-right.svg" alt=""></img>',
         asNavFor: '.slider-map',
-    })
+        //дописываем респонсив
+        responsive: [{
+                breakpoint: 1210,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+
+                }
+            },
+        ]
+
+    });
     $('.slider-map').slick({
         slidesToShow: 8,
         slidesToScroll: 1,
         arrows: false,
         asNavFor: '.surf-slider', //связать слайдеры
         focusOnSelect: true,
+        responsive: [{
+                breakpoint: 1103,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: { slidesToShow: 2, },
+            }
+        ]
     });
-    $('.holder__slider').slick({
+    $('.holder__slider,.shop__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left"  src="assets/img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="assets/img/arrows-right.svg" alt=""></img>',
     })
-});
-$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="assets/img/plus.svg " alt=" "></div><div class="quantity-button quantity-down"><img src="assets/img/minus.svg " alt=" "></div></div>').insertAfter('.quantity input');
-$('.quantity').each(function() {
-    var spinner = jQuery(this),
-        input = spinner.find('input[type="number"]'),
-        btnUp = spinner.find('.quantity-up'),
-        btnDown = spinner.find('.quantity-down'),
-        min = input.attr('min'),
-        max = input.attr('max');
 
-    btnUp.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue >= max) {
-            var newVal = oldValue;
-        } else {
-            var newVal = oldValue + 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-    });
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="assets/img/plus.svg " alt=" "></div><div class="quantity-button quantity-down"><img src="assets/img/minus.svg " alt=" "></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+        var spinner = jQuery(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
 
-    btnDown.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue <= min) {
-            var newVal = oldValue;
-        } else {
-            var newVal = oldValue - 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
     });
     $('.quantity-button').on('click', function() {
         let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
         $('.summ').html('$' + summ);
-    })
+    });
     let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
     $('.summ').html('$' + summ);
-});;
+
+    $('.surfboard-box__circle').on('click', function() {
+        $(this).toggleClass('active')
+    });
+
+    $('.menu-btn').on('click', function() {
+        $('.menu').toggleClass('active');
+
+    });
+    //анимация при скролее wow.js сайт
+    new WOW().init();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const autoprefixer = require('autoprefixer');
 const mqpacker = require('css-mqpacker');
 const sortCSSmq = require('sort-css-media-queries');
 
+
 const path = {
     scssFolder: './assets/scss/',
     scssFiles: './assets/scss/**/*.scss',
@@ -83,13 +84,26 @@ task('scss', series(scss));
 task('dev', series(scssDev));
 task('watch', watchFiles);
 
-// gulp.task('css', function() {
-//     return gulp.src([
-//             'node_modules/normalize.css/normalize.css',
-//             'node_modules/slick-carousel/slick/slick.css',
-//             'node_modules/animate.css/animate.scc'
-//         ])
-//         .pipe(concat('_libs.scss'))
-//         .pipe(gulp.dest('app/scss'))
-//         .pipe(browserSync.reload({ stream: true }))
-// });
+let gulp = require('gulp')
+
+gulp.task('css', function() {
+    return gulp.src([
+            'node_modules/normalize.css/normalize.css',
+            'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/animate.css/animate.css',
+        ])
+        // .pipe(concat('_libs.scss'))
+        .pipe(gulp.dest('assets/scss'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/wow.js/dist/wow.js'
+        ])
+        .pipe(concat('assets/js/libs.min.js'))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(browserSync.reload({ stream: true }))
+})
